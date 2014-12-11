9ca40d5ab1cda485320e5b78c0ec90144aab683a
MuriloMielke <murilo@baixou.com.br>
2014-12-11 17:00:31 -0200

    add facebook button to login using it

--- index_facebook.js
+++ index_facebook.js
@@ -1,6 +1,14 @@
+/* =============================================================
+	Config
+============================================================= */ 
+
 var myApp = '259817854185940';
 var albumNow = '1530024997270481'; // id of the 'current' album
 var albumOld = '1530187357254245'; // id of the 'past submission' album
+
+/* =============================================================
+	Includes
+============================================================= */ 
 
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -10,59 +18,87 @@ var albumOld = '1530187357254245'; // id of the 'past submission' album
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+/* =============================================================
+	Actions
+============================================================= */ 
 
-function removeAPP() {
+function showLoginAPP() {
+	$('.divCheckingFacebook').hide();
+	$('.divButtonFacebook').fadeIn(200);
+}
+
+function loginAPP() {
+	FB.login(loginCallback,{scope: 'user_groups, user_photos'});
+}
+
+function logoutAPP() {
 	FB.api('/me/permissions', 'delete', function(response) {
     	location.reload();
 	});
 }
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: myApp,
-        xfbml: true, version: 'v2.2'
-    });
+/* =============================================================
+	Flux
+============================================================= */ 
 
-    aToken = null;
+function getStatusCallback(response) {
+	console.log('getStatusCallback',response);
+    if (response.status === 'connected') getData(response);
+    else showLoginAPP();
+}
 
-	function getStatusCallback(response) {
-		console.log('getStatusCallback',response);
-	    if (response.status === 'connected') getData(response);
-	    else FB.login(loginCallback,{scope: 'user_photos'});
+function loginCallback(response) {
+	console.log('loginCallback',response);
+	if (response.status == 'connected') {
+		$('.divButtonFacebook').hide();
+		$('.divLoadingAlbum').show();
+		getData(response);
 	}
+	if (response.status == 'not_authorized') location.href = 'notlogged.html';
+}
 
-	function loginCallback(response) {
-		console.log('loginCallback',response);
-		if (response.status == 'connected') getData(response);
-		if (response.status == 'not_authorized') location.href = 'notlogged.html';
-	}
+/* =============================================================
+	Requesting
+============================================================= */ 
 
-	function getData(response) {
-	    aToken = response.authResponse.accessToken;
-	    sigRequest = response.authResponse.signedRequest;
-		getPictures(albumNow,'#current');
-		getPictures(albumOld,'#past');
-	}
+function getData(response) {
+	$('.divCheckingFacebook').hide();
+	$('.divLoadingAlbum').show();
+	console.log('getData()');
+    aToken = response.authResponse.accessToken;
+    sigRequest = response.authResponse.signedRequest;
+	getPictures(albumNow,'#current');
+	getPictures(albumOld,'#past');
+}
 
-
-	function getPictures(idGallery,selector) {
-		FB.api("/"+idGallery+"/photos?signed_request="+sigRequest+"&access_token="+aToken, function (response) {
-			if (response && !response.error) {
-				console.log('getPictures',response.data);
-				if (selector) {
-					var $ul = $(selector+' .rslides').empty();
-					for (i in response.data) {
-						var photo = response.data[i];
-						var $li = $("<li>").appendTo($ul);
-						var $img = $("<img>",{'src':photo.source}).appendTo($li);
-					}
-					$(selector).carousel({});
-				    $(selector+' .rslides').responsiveSlides({pager: true, nav: true, pause: true});
+function getPictures(idGallery,selector) {
+	console.log('getPictures('+idGallery+','+selector+')');
+	FB.api("/"+idGallery+"/photos?signed_request="+sigRequest+"&access_token="+aToken, function (response) {		
+		if (response && !response.error) {
+			$(selector+' .divLoadingAlbum').hide();
+			console.log('getPictures',response.data);
+			if (selector) {
+				var $ul = $(selector+' .rslides').empty();
+				for (i in response.data) {
+					var photo = response.data[i];
+					var $li = $("<li>").appendTo($ul);
+					var $img = $("<img>",{'src':photo.source}).appendTo($li);
 				}
+				$(selector).carousel({});
+			    $(selector+' .rslides').responsiveSlides({pager: true, nav: true, pause: true});
 			}
-		});
-	}
+		} else {
+			$(selector+' .divLoadingAlbum').show().html(response.error.message);
+		}
+	});
+}
 
+/* =============================================================
+	Init
+============================================================= */ 
+
+window.fbAsyncInit = function () {
+    FB.init({appId: myApp, xfbml: true, version: 'v2.2'});
     FB.getLoginStatus(getStatusCallback);
 };
 
